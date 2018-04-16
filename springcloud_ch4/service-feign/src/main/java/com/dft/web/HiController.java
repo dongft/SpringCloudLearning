dd01e1e2e198266ba95dfa78207f338738b6060a
dongfuting <dongfuting@hdsc.com>
2018-04-16 17:48:10 +0800

    完成ch4学习

---
+++ springcloud_ch4/service-feign/src/main/java/com/dft/web/HiController.java
@@ -0,0 +1,24 @@
+package com.dft.web;
+
+import com.dft.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Class Name HiController
+ * @Author dongfuting
+ * @Create In 2018-04-16 0:28
+ */
+@RestController
+public class HiController {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClient(name);
+    }
+}
---
+++ springcloud_ch4/service-ribbon/src/main/java/com/dft/ServiceRibbonApplication.java
@@ -0,0 +1,32 @@
+package com.dft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Class Name ServiceRibbonApplication
+ * @Author dongfuting
+ * @Create In 2018年04月13日 16:35
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+public class ServiceRibbonApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceRibbonApplication.class, args);
+  }
+
+  @Bean
+  @LoadBalanced
+  RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+}
