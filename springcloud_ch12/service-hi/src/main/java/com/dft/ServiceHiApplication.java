516fab4e75a4979c72252469970b23d1c6532410
dongfuting <dongfuting@hdsc.com>
2018-04-27 17:46:23 +0800

    完成ch12学习

---
+++ springcloud_ch12/service-hi/src/main/java/com/dft/ServiceHiApplication.java
@@ -0,0 +1,42 @@
+package com.dft;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Class Name ServiceHiApplication
+ * @Author dongfuting
+ * @Create In 2018年04月12日 17:34
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+public class ServiceHiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceHiApplication.class, args);
+  }
+
+  @Value("${server.port}")
+  String port;
+
+  @RequestMapping("/hi")
+  @HystrixCommand(fallbackMethod = "hiError")
+  public String home(@RequestParam String name) {
+    return "hi " + name + ",i am from port:" + port;
+  }
+
+  public String hiError(String name) {
+    return "hi," + name + ",sorry,error.";
+  }
+}
